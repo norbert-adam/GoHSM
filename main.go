e5fafd47a2a207b4f9585fb33ecc76dc8fb5e80b
punisher <norbert@punisher>
2025-10-05 15:07:04 +0200

    Implement listing of available slots

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 // TODO: Read config file to get the library paths
-// 
+// TODO: Print error messages to stderr
 
 
 import (
@@ -22,4 +22,25 @@ func main() {
 	defer p.Finalize()
 
 	fmt.Println("PKCS11 Module successfully initialized!")
+	
+	slots, err := p.GetSlotList(true)
+	if err != nil {
+		fmt.Println("Error listing slots: ", err)
+		return
+	}
+
+	fmt.Println("Available slots:")
+	for i, slot := range slots {
+		tInfo, err := p.GetTokenInfo(slot)
+		if err != nil {
+			fmt.Println("Error loading token info: ", err)
+		}
+		var label string
+		if tInfo.Label == "" {
+			label = "N/A"
+		} else {
+			label = tInfo.Label
+		}
+		fmt.Printf("\t%d. Slot: %s (%d)\n", i, label, slot)
+	}
 }
