79bc0c2bb33e7b15fdf4df3ce281b26fa03ae7c0
punisher <norbert@punisher>
2025-10-16 17:32:25 +0200

    Minor refactorings

--- objects/objects.go
+++ objects/objects.go
@@ -36,6 +36,7 @@ func ListObjects(p *context.AppContext, searchOption string) ([]pkcs11.ObjectHan
 		objectType, err := getObjectType(p, objs[0])
 		if err != nil {
 			return nil, err
+
 		}
 
 		switch searchOption {
@@ -53,4 +54,36 @@ func ListObjects(p *context.AppContext, searchOption string) ([]pkcs11.ObjectHan
 	}
 	
 	return foundObjs, nil
+}
+
+func SelectObject(p *context.AppContext, objectList []pkcs11.ObjectHandle) (pkcs11.ObjectHandle, error) {
+	
+	var selection int
+	fmt.Println("Select the object: ")
+	for i, obj := range objectList {
+		objType, err := getObjectType(p, obj)
+		if err != nil {
+			return 0, err
+		}
+		attrTemplate := getAttributeTemplate("short", objType)
+
+		attrs, err := p.P11.GetAttributeValue(p.Session, obj, attrTemplate)
+		if err != nil {
+			newErr := fmt.Sprint("Error getting attributes: ", err)
+			return 0, errors.New(newErr)
+		}
+		fmt.Printf("%d. Object: %v (object handle: %d / label %s)\n", i, AttrToString(attrs[0]), obj, attrs[1].Value)
+	}
+
+	_, err := fmt.Scan(&selection)
+	if err != nil {
+		newErr := fmt.Sprintf("Incorrect input: %v", err)
+		return 0, errors.New(newErr)
+	}
+	if selection < 0 || selection > len(objectList) {
+		newErr := fmt.Sprintf("incorrect input - selected option must be between 0 and %d", len(objectList)-1)
+		return 0, errors.New(newErr)
+	}
+
+	return objectList[selection], nil
 }
--- objects/ui.go
+++ objects/ui.go
@@ -3,14 +3,10 @@ package objects
 import (
 	"fmt"
 	"errors"
-
-	"github.com/GoHSM/context"
-
-	"github.com/miekg/pkcs11"
 )
 
 
-func selectOption() (string, error) {
+func selectObjectType() (string, error) {
 
 	options := map[int]string{
 		1: "All",
@@ -50,7 +46,10 @@ func selectAction() (string, error) {
 		8: "Exit",
 	}
 
-	fmt.Printf("Select the type of option you want to list:\n")
+	fmt.Println("")
+	fmt.Println(" ----------------------------")
+	fmt.Printf("| SELECT WHAT YOU WANT TO DO |\n")
+	fmt.Println(" ----------------------------")
 	fmt.Printf("\t1. Delete\n")
 	fmt.Printf("\t2. Encrypt\n")
 	fmt.Printf("\t3. Decrypt\n")
@@ -67,42 +66,9 @@ func selectAction() (string, error) {
 		return "", errors.New(newErr)
 	}
 	if selection < 1 || selection > len(options) {
-		// TODO: replace hard-coded number with the last key of the map
-		return "", errors.New("incorrect input - selected option must be between 1 and 8")
+		newErr := fmt.Sprintf("Incorrect input - selected option must be between 1 and %d", len(options))
+		return "", errors.New(newErr)
 	}
 
 	return options[selection], nil
-}
-
-
-func SelectObject(p *context.AppContext, objectList []pkcs11.ObjectHandle) (pkcs11.ObjectHandle, error) {
-	
-	var selection int
-	fmt.Println("Select the object: ")
-	for i, obj := range objectList {
-		objType, err := getObjectType(p, obj)
-		if err != nil {
-			return 0, err
-		}
-		attrTemplate := getAttributeTemplate("short", objType)
-
-		attrs, err := p.P11.GetAttributeValue(p.Session, obj, attrTemplate)
-		if err != nil {
-			newErr := fmt.Sprint("Error getting attributes: ", err)
-			return 0, errors.New(newErr)
-		}
-		fmt.Printf("%d. Object: %v (object handle: %d / label %s)\n", i, AttrToString(attrs[0]), obj, attrs[1].Value)
-	}
-
-	_, err := fmt.Scan(&selection)
-	if err != nil {
-		newErr := fmt.Sprintf("Incorrect input: %v", err)
-		return 0, errors.New(newErr)
-	}
-	if selection < 0 || selection > len(objectList) {
-		newErr := fmt.Sprintf("incorrect input - selected option must be between 0 and %d", len(objectList)-1)
-		return 0, errors.New(newErr)
-	}
-
-	return objectList[selection], nil
 }
